e8df448f7db2bf0c9d177922b653c02455d63a8a
Yorick Ferlin <yorick.ferlin@bedrockstreaming.com>
2023-03-01 11:15:49 +0100

    minor changes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,39 +1,38 @@
 -- Learn the keybindings, see :help lsp-zero-keybindings
 -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'lua_ls',
-    'dockerls',
-    'sqlls',
-    'jsonls',
-    'marksman',
-    'yamlls',
-    'rust_analyzer',
+    "tsserver",
+    "eslint",
+    "lua_ls",
+    "dockerls",
+    "sqlls",
+    "jsonls",
+    "yamlls",
+    "rust_analyzer",
 })
 
-lsp.configure('lua-language-server', {
+lsp.configure("lua-language-server", {
     settings = {
         Lua = {
             diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
+                globals = { "vim" },
+            },
+        },
+    },
 })
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = nil,
-    ['<S-Tab>'] = nil
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<Tab>"] = nil,
+    ["<S-Tab>"] = nil,
 })
 
 lsp.setup_nvim_cmp({
@@ -50,16 +49,34 @@ lsp.setup_nvim_cmp({
 local function config(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<C-h>", function()
+        vim.lsp.buf.signature_help()
+    end, opts)
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { desc = "[G]o [D]efinition" })
-    vim.keymap.set("n", "fmt", function() vim.lsp.buf.format({ async = true }) end, { desc = "[F]or[M]a[T]" })
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, { desc = "[V]iew [D]iagnostic" })
-    vim.keymap.set("n", "$d", function() vim.diagnostic.goto_next() end, { desc = "Next occurrence" })
-    vim.keymap.set("n", "ùd", function() vim.diagnostic.goto_prev() end, { desc = "Previous occurence" })
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, { desc = "[V]iew [C]ode [A]ction" })
+    vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+    end, { desc = "[G]o [D]efinition" })
+    vim.keymap.set("n", "fmt", function()
+        vim.lsp.buf.format({ async = true })
+    end, { desc = "[F]or[M]a[T]" })
+    vim.keymap.set("n", "K", function()
+        vim.lsp.buf.hover()
+    end)
+    vim.keymap.set("n", "<leader>vws", function()
+        vim.lsp.buf.workspace_symbol()
+    end)
+    vim.keymap.set("n", "<leader>vd", function()
+        vim.diagnostic.open_float()
+    end, { desc = "[V]iew [D]iagnostic" })
+    vim.keymap.set("n", "$d", function()
+        vim.diagnostic.goto_next()
+    end, { desc = "Next occurrence" })
+    vim.keymap.set("n", "ùd", function()
+        vim.diagnostic.goto_prev()
+    end, { desc = "Previous occurence" })
+    vim.keymap.set("n", "<leader>vca", function()
+        vim.lsp.buf.code_action()
+    end, { desc = "[V]iew [C]ode [A]ction" })
     vim.keymap.set("n", "<leader>vco", function()
         vim.lsp.buf.code_action({
             filter = function(code_action)
@@ -70,64 +87,58 @@ local function config(client, bufnr)
                 local data = code_action.data.id
                 return string.sub(data, #data - 1, #data) == ":0"
             end,
-            apply = true
+            apply = true,
         })
     end, { desc = "[V]iew [C]ode actions [O]thers" })
-    vim.keymap.set("n", "<leader>vcr", function() vim.lsp.buf.references() end, { desc = "[V]iew [C]ode [R]eferences" })
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, { desc = "[V]iew [R]e[N]ame" })
+    vim.keymap.set("n", "<leader>vcr", function()
+        vim.lsp.buf.references()
+    end, { desc = "[V]iew [C]ode [R]eferences" })
+    vim.keymap.set("n", "<leader>vrn", function()
+        vim.lsp.buf.rename()
+    end, { desc = "[V]iew [R]e[N]ame" })
 end
 
 local function merge_config(_config, config)
     return vim.tbl_deep_extend("force", {
-        on_attach = config()
+        on_attach = config(),
     }, _config or {})
 end
 
 lsp.on_attach(config())
 
 -- then for each specific language, we overload the config
-require("lspconfig").phpactor.setup(
-    merge_config(
-        {
-            init_options = {
-                ["language_server_phpstan.enabled"] = false,
-                ["language_server_psalm.enabled"] = false,
+require("lspconfig").phpactor.setup(merge_config({
+    init_options = {
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
+    },
+
+    on_attach = function()
+        print("phpactor loaded")
+        vim.keymap.set("n", "gd", ":PhpactorGotoDefinition<CR>")
+        vim.keymap.set("n", "gdv", ":PhpactorGotoDefinition vsplit<CR>")
+        vim.keymap.set("n", "<leader>j", ":PhpactorImportClass<CR>")
+        vim.keymap.set("n", "<leader>cf", ":PhpactorCopyFile<CR>")
+        vim.keymap.set("n", "<leader>cc", ":PhpactorCopyClassName<CR>")
+        -- find usages in quifix list
+        vim.keymap.set("n", "<leader>fr", ":PhpactorFindReferences<CR>")
+        -- jump to parent class
+        vim.keymap.set("n", "<leader>nv", ":PhpactorNavigate<CR>")
+    end,
+
+    filetypes = { "php", "cucumber" },
+}, config))
+
+require("lspconfig").gopls.setup(merge_config({
+    cmd = { "gopls", "serve" },
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
             },
-
-            on_attach = function()
-                print('phpactor loaded')
-                vim.keymap.set("n", "gd", ":PhpactorGotoDefinition<CR>")
-                vim.keymap.set("n", "gdv", ":PhpactorGotoDefinition vsplit<CR>")
-                vim.keymap.set("n", "<leader>j", ":PhpactorImportClass<CR>")
-                vim.keymap.set("n", "<leader>cf", ":PhpactorCopyFile<CR>")
-                vim.keymap.set("n", "<leader>cc", ":PhpactorCopyClassName<CR>")
-                -- find usages in quifix list
-                vim.keymap.set("n", "<leader>fr", ":PhpactorFindReferences<CR>")
-                -- jump to parent class
-                vim.keymap.set("n", "<leader>nv", ":PhpactorNavigate<CR>")
-            end,
-
-            filetypes = { 'php', 'cucumber' },
+            staticcheck = true,
         },
-        config
-    )
-)
-
-require("lspconfig").gopls.setup(
-    merge_config(
-        {
-            cmd = { "gopls", "serve" },
-            settings = {
-                gopls = {
-                    analyses = {
-                        unusedparams = true,
-                    },
-                    staticcheck = true,
-                },
-            },
-        },
-        config
-    )
-)
+    },
+}, config))
 
 lsp.setup()
