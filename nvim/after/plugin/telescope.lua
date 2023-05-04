2704216ec195bed8eb1b6db7ee364dc49747c131
Yorick <yorickferlin@gmail.com>
2023-05-04 07:56:54 +0200

    update telescope to live_grep all

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local ok, telescope = pcall(require, "telescope")
 if not ok then
-    print(string.format("Someting happend on calling telescope in %s", vim.fn.expand('%:p')))
+    print(string.format("Someting happend on calling telescope in %s", vim.fn.expand("%:p")))
 end
 
 telescope.setup({
     defaults = {
-        file_ignore_patterns = { "%.git/" }
+        file_ignore_patterns = { "%.git/" },
     },
     {
         extensions = {
@@ -16,17 +16,15 @@ telescope.setup({
                 case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             },
             ["ui-select"] = {
-                require("telescope.themes").get_dropdown(
-                    {
-                        layout_strategy = "vertical",
-                        layout_config = "bottom",
-                        prompt_position = "bottom",
-                        vertical = {
-                            width = 0.5,
-                            height = 20,
-                        }
-                    }
-                )
+                require("telescope.themes").get_dropdown({
+                    layout_strategy = "vertical",
+                    layout_config = "bottom",
+                    prompt_position = "bottom",
+                    vertical = {
+                        width = 0.5,
+                        height = 20,
+                    },
+                }),
             },
         },
         pickers = {
@@ -37,25 +35,28 @@ telescope.setup({
             live_grep = {
                 additional_args = function(opts)
                     return { "--hidden" }
-                end
+                end,
             },
-        }
-    }
+        },
+    },
 })
-telescope.load_extension('fzf')
-telescope.load_extension('ui-select')
+telescope.load_extension("fzf")
+telescope.load_extension("ui-select")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ps", function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set("n", '<leader>ff', builtin.find_files, { desc = "[F]ind in all [F]iles" })
-vim.keymap.set("n", '<leader>fg', builtin.git_files, { desc = "[F]ind in [G]it files" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind in all [F]iles" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "[F]ind in [G]it files" })
 vim.keymap.set("n", "<leader>ç", builtin.oldfiles, { desc = "[ç] Find recently opened files" })
-vim.keymap.set("n", '<leader><space>', builtin.buffers, { desc = "Find Buffers" })
-vim.keymap.set("n", '<leader>fh', builtin.help_tags, { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Find Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymap" })
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "[L]ive [G]rep" })
+vim.keymap.set("n", "<leader>lga", function()
+    builtin.live_grep({ cwd = vim.fn.expand("%:p:h") })
+end, { desc = "[L]ive [G]rep" })
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 vim.keymap.set("n", "<leader>fs", builtin.treesitter, { desc = "[F]ind [S]ymbols" })
 vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "[G]oto [R]eferences" })
