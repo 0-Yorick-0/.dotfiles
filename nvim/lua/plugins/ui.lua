1c6dc2da7e182147ed853e435861f35dd4550a6a
Yorick Ferlin <yorick.ferlin@bedrockstreaming.com>
2023-06-15 18:08:18 +0200

    add block plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,43 +1,50 @@
 return {
-    {
-        "glepnir/dashboard-nvim",
-        event = "VimEnter",
-    },
-    {
-        "folke/noice.nvim",
-        config = function()
-            require("noice").setup({
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true,
-                    },
-                },
-                messages = {
-                    enabled = false,
-                },
-            })
-        end,
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
-        },
-    },
-    {
-        -- Set lualine as statusline
-        -- See `:help lualine.txt`
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup({
-                options = {
-                    icons_enabled = false,
-                    theme = "auto",
-                    component_separators = "|",
-                    section_separators = "",
-                },
-            })
-        end,
-    },
+	{
+		"glepnir/dashboard-nvim",
+		event = "VimEnter",
+	},
+	{
+		"folke/noice.nvim",
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				messages = {
+					enabled = false,
+				},
+			})
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
+	{
+		-- Set lualine as statusline
+		-- See `:help lualine.txt`
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					theme = "auto",
+					component_separators = "|",
+					section_separators = "",
+				},
+			})
+		end,
+	},
+	{
+		"HampusHauffman/block.nvim",
+		config = function()
+			require("block").setup({})
+			vim.keymap.set("n", "<leader>b", ":Block<cr>")
+		end,
+	},
 }
