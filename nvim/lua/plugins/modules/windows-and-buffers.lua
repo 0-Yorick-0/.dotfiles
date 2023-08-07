e5bab0a53b11d39a1ff32fb401bc8a1139c91567
Yorick Ferlin <yorick.ferlin@bedrockstreaming.com>
2023-08-07 10:09:38 +0200

    minor updates

--- nvim/lua/plugins/modules/windows-and-buffers.lua
+++ nvim/lua/plugins/modules/windows-and-buffers.lua
@@ -1,6 +1,9 @@
 return {
     {
         "nvim-focus/focus.nvim",
+        dependencies = {
+            "echasnovski/mini.test",
+        },
         config = function()
             vim.keymap.set("n", "<leader>=", ":FocusMaxOrEqual<CR>", { silent = true })
             vim.keymap.set("n", "<leader>o", ":FocusMaximise<CR>", { silent = true })
