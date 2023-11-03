623fa0e7692a7b0ce7066e39bdbb36edfd33504a
Yorick Ferlin <yorick.ferlin@bedrockstreaming.com>
2023-11-03 17:09:07 +0100

    make focu VeryLazy

--- nvim/lua/plugins/windows-and-buffers.lua
+++ nvim/lua/plugins/windows-and-buffers.lua
@@ -1,10 +1,11 @@
 return {
-    {
-        "nvim-focus/focus.nvim",
-        config = function()
-            vim.keymap.set("n", "<leader>=", ":FocusMaxOrEqual<CR>", { silent = true })
-            vim.keymap.set("n", "<leader>o", ":FocusMaximise<CR>", { silent = true })
-            require("focus").setup()
-        end,
-    },
+	{
+		"nvim-focus/focus.nvim",
+		event = "VeryLazy",
+		config = function()
+			vim.keymap.set("n", "<leader>=", ":FocusMaxOrEqual<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>o", ":FocusMaximise<CR>", { silent = true })
+			require("focus").setup()
+		end,
+	},
 }
