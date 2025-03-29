23de29c7feff1c8115c4b30748a1e24319961171
Yorick <admin@Mac.lan>
2025-03-29 16:59:29 +0100

    create dynamic nvim configs

--- install/install-vim.sh
+++ install/install-vim.sh
@@ -9,7 +9,7 @@ brew install stylua
 brew install gofumpt
 brew install luarocks
 
-MY_NEOVIM=~/.config/my-neovim
+MY_NEOVIM=~/.config/nvim-default
 export MY_NEOVIM
 
 rm -rf "$MY_NEOVIM"
@@ -17,6 +17,7 @@ rm -rf "$MY_NEOVIM"
 mkdir -p "$MY_NEOVIM"/share
 mkdir -p "$MY_NEOVIM"/nvim
 
+# create symlinks between .dotfiles.nvim and ~/.config/nvim thanks to stow
 stow --restow --target="$MY_NEOVIM"/nvim nvim
 
 alias mnv='XDG_DATA_HOME=$MY_NEOVIM/share XDG_CACHE_HOME=$MY_NEOVIM XDG_CONFIG_HOME=$MY_NEOVIM nvim'
@@ -35,10 +36,10 @@ ln -sF "$DOTFILES/nvim/after" "$MY_NEOVIM"
 mkdir -p "$MY_NEOVIM/undo"
 
 # phpactor config file
-if [[ ! -d "$XDG_CONFIG_HOME/my-neovim/share/nvim/mason/packages/phpactor" ]]; then
-	mkdir "XDG_CONFIG_HOME/my-neovim/share/nvim/mason/packages/phpactor"
+if [[ ! -d "$XDG_CONFIG_HOME/nvim-default/share/nvim/mason/packages/phpactor" ]]; then
+	mkdir "XDG_CONFIG_HOME/nvim-default/share/nvim/mason/packages/phpactor"
 fi
-ln -sf "$DOTFILES/phpactor/phpactor.json" "$XDG_CONFIG_HOME/my-neovim/share/nvim/mason/packages/phpactor/phpactor.json"
+ln -sf "$DOTFILES/phpactor/phpactor.json" "$XDG_CONFIG_HOME/nvim-default/share/nvim/mason/packages/phpactor/phpactor.json"
 
 # yamlfmt config file
 if [ ! -d "$XDG_CONFIG_HOME/yamlfmt" ]; then
