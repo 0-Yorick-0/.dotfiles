f6fe1b42333c1fe3243572a45810a5ab20387a4d
Yorick Ferlin <yorick.ferlin@bedrockstreaming.com>
2025-04-03 09:17:19 +0200

    update ui

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -13,16 +13,21 @@ return {
 		"folke/noice.nvim",
 		config = function()
 			require("noice").setup({
-				-- routes = {
-				-- 	{
-				-- 		filter = {
-				-- 			event = "msg_show",
-				-- 			kind = "",
-				-- 			find = "written",
-				-- 		},
-				-- 		opts = { skip = true },
-				-- 	},
-				-- },
+				cmdline = {
+					format = {
+						input = { view = "cmdline" },
+					},
+				},
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							kind = "",
+							find = "written",
+						},
+						opts = { skip = true },
+					},
+				},
 				lsp = {
 					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 					override = {
