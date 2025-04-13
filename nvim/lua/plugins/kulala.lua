4a252f95e2dc9d774d0109d0486062b1e901c054
Yorick <admin@Mac.lan>
2025-04-13 19:27:13 +0200

    add kulala api client

---
+++ nvim/lua/plugins/kulala.lua
@@ -0,0 +1,16 @@
+-- https://neovim.getkulala.net/
+return {
+	{
+		"mistweaverco/kulala.nvim",
+		keys = {
+			{ "<leader>Rs", desc = "Send request" },
+			{ "<leader>Ra", desc = "Send all requests" },
+			{ "<leader>Rb", desc = "Open scratchpad" },
+		},
+		ft = { "http", "rest", "graphql" },
+		opts = {
+			-- your configuration comes here
+			global_keymaps = true,
+		},
+	},
+}
